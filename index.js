8079e8e1ff4d1fb7e5c5533c941f3ae7823fdcaa
robdy <robdyjas@gmail.com>
2019-02-14 23:16:30 +0100

    Add low five

--- index.js
+++ index.js
@@ -1,74 +1,107 @@
 const Discord = require('discord.js');
-const client  = new Discord.Client();
-const config  = require('./config.js');
+const client = new Discord.Client();
+const config = require('./config.js');
 const twitter = require('twit');
- 
-const twitterClient = new twitter( config.twitter );
+const moment = require('moment');
 
-if ( config.isDev === "Y" ) { config.twitterUsersToFollow.push('2899773086') } // @Every3Minutes
+// Connecting to Twitter
+const twitterClient = new twitter(config.twitter);
 
-const stream = twitterClient.stream('statuses/filter', { follow: config.twitterUsersToFollow });
+const isDev = (config.isDev === 'Y');
+
+if (isDev) {
+  config.twitterUsersToFollow.push('2899773086')
+} // add @Every3Minutes to follow list for testing
+
+// Create a stream to follow tweets
+const stream = twitterClient.stream('statuses/filter', {
+  follow: config.twitterUsersToFollow
+});
 
 // https://github.com/ttezel/twit/issues/286#issuecomment-236315960
 function isReply(tweet) {
-  if ( tweet.retweeted_status
-    || tweet.in_reply_to_status_id
-    || tweet.in_reply_to_status_id_str
-    || tweet.in_reply_to_user_id
-    || tweet.in_reply_to_user_id_str
-    || tweet.in_reply_to_screen_name )
+  if (tweet.retweeted_status ||
+    tweet.in_reply_to_status_id ||
+    tweet.in_reply_to_status_id_str ||
+    tweet.in_reply_to_user_id ||
+    tweet.in_reply_to_user_id_str ||
+    tweet.in_reply_to_screen_name)
     return true
 }
 
 function isOwnRetweet(tweet) {
-  if ( tweet.retweeted_status && config.twitterUsersToFollow.includes( tweet.user.id_str ))
+  if (tweet.retweeted_status && config.twitterUsersToFollow.includes(tweet.user.id_str))
     return true;
 }
 
-stream.on('tweet', function (tweet) {
+stream.on('tweet', function(tweet) {
   // Exclude replies and retweet but not own ones
-  if ( isReply(tweet) && !(isOwnRetweet(tweet)) ) return true;
-  
+  if (isReply(tweet) && !(isOwnRetweet(tweet))) return true;
+
   // On dev, log tweets to console
-  if(config.isDev === 'Y') console.log(tweet);
-  
-  const twitterMessage = tweet.user.name + ' (@' + tweet.user.screen_name + ') ' + ( isOwnRetweet(tweet) ? 're' : '' ) + 'tweeted this at ' + tweet.created_at + ': https://twitter.com/' + tweet.user.screen_name + '/status/' + tweet.id_str;
+  if (isDev) console.log(tweet);
+
+  const twitterMessage = tweet.user.name + ' (@' + tweet.user.screen_name + ') ' + (isOwnRetweet(tweet) ? 're' : '') + 'tweeted this at ' + moment(tweet.created_at, 'dd MMM DD HH:mm:ss ZZ YYYY', 'en').format('YYYY-MM-DD HH:mm') + ': https://twitter.com/' + tweet.user.screen_name + '/status/' + tweet.id_str;
   for (let i = 0; i < config.updateChannels.length; i++) {
-    client.channels.get( config.updateChannels[i] ).send( twitterMessage );
+    client.channels.get(config.updateChannels[i]).send(twitterMessage);
   }
 })
 
 client.on('ready', () => {
   // Presence settings
-  const game = '@Elekid bot' + (config.isDev === 'Y' ? ' - dev' : '');
-  client.user.setPresence({ status: 'online', game: { name: game} });
+  const game = '@Elekid bot' + (isDev ? ' - dev' : '');
+  client.user.setPresence({
+    status: 'online',
+    game: {
+      name: game
+    }
+  });
   console.log('I\'m in');
   console.log(client.user.username);
-  
-  // Tasks
-   //setInterval(executeTask, 10 * 1000);
 });
-  
+
 client.on('message', async msg => {
   // Ignore bots
-  if(msg.author.bot) return;
-  
+  if (msg.author.bot) return;
+
   // High five
   if (/[oO0]\/(?!\\[oO0])/.exec(msg.content)) {
     const sender = msg.author;
-    const filter = m => (/(?<![oO0]\/)\\[oO0]/.exec(m.content) && (config.isDev === 'Y' ? true : m.author.id !== sender.id) && !(m.author.bot));
-    await msg.channel.awaitMessages(filter, { max:1, time: 120000, errors: ['time'] })
-    .then( msgs => {
-      const msg = msgs.first();
-      const receiver = msg.author;
-      pingMsg = `${sender} o/\\o ${receiver}`;
-      msg.channel.send(pingMsg);
-    })
-    .catch(err => console.log('High five from ' + sender.username + ' timeout'));
+    const filter = m => (/(?<![oO0]\/)\\[oO0]/.exec(m.content) && (isDev ? true : m.author.id !== sender.id) && !(m.author.bot));
+    await msg.channel.awaitMessages(filter, {
+        max: 1,
+        time: 120000,
+        errors: ['time']
+      })
+      .then(msgs => {
+        const msg = msgs.first();
+        const receiver = msg.author;
+        pingMsg = `${sender} o/\\o ${receiver}`;
+        msg.channel.send(pingMsg);
+      })
+      .catch(err => console.log('High five from ' + sender.username + ' timeout'));
+  }
+  
+  // Low five
+  if (/[oO0]\\(?!\/[oO0])/.exec(msg.content)) {
+    const sender = msg.author;
+    const filter = m => (/(?<![oO0]\\)\/[oO0]/.exec(m.content) && (isDev ? true : m.author.id !== sender.id) && !(m.author.bot));
+    await msg.channel.awaitMessages(filter, {
+        max: 1,
+        time: 120000,
+        errors: ['time']
+      })
+      .then(msgs => {
+        const msg = msgs.first();
+        const receiver = msg.author;
+        pingMsg = `${sender} o\\\\/o ${receiver}`;
+        msg.channel.send(pingMsg);
+      })
+      .catch(err => console.log('Low five from ' + sender.username + ' timeout'));
   }
 
   // Respond to mention
-  const didYouMentionMe = msg => msg.mentions.users.some( mention => mention.id === client.user.id);
+  const didYouMentionMe = msg => msg.mentions.users.some(mention => mention.id === client.user.id);
   if (didYouMentionMe(msg)) {
     // embed generator: https://leovoel.github.io/embed-visualizer/
     const embed = {
@@ -87,23 +120,24 @@ client.on('message', async msg => {
         'name': 'Hello, I am Elekid bot',
         'icon_url': 'https://cdn.discordapp.com/avatars/512740516371234826/789d4750ee72c08f06f38065b8923eec.png?size=40'
       },
-      'fields': [
-        {
+      'fields': [{
           'name': 'High five o/\\o',
-          'value': '- Send someone high five: `o/`\n- Respond with `\\o`'
+          'value': 'Send with `o/`and respond with `\\o`'
         },
         {
-          'name': 'News',
-          'value': '- Are being sent to predefined channels'
+          'name': 'Low five o\\\\/o',
+          'value': 'Send with `o\\`and respond with `/o`'
         },
         {
-          'name': 'Help',
-          'value': '- Ping me to see this message'
-        }
+          'name': 'Others',
+          'value': 'News - are being sent to predefined channels\nHelp - Ping me to see this message'
+        }         
       ]
     };
-    msg.channel.send({ embed });
-  } 
+    msg.channel.send({
+      embed
+    });
+  }
 });
 
 client.login(config.token);
