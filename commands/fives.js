79ba75c22fc298829260531a552a5ea6b93252e2
robdy <robdyjas@gmail.com>
2019-07-11 22:19:19 +0200

    Refactor fives

---
+++ commands/fives.js
@@ -0,0 +1,98 @@
+const config = require('../config.js');
+const giphyFunctions = require('../services/giphy.js');
+
+let pendingFives = [];
+const isDev = (config.isDev === 'Y');
+
+
+const fives = {
+  'high-five': {
+    left: /[oO0]\/(?!\\[oO0])/,
+    right: /(?<![oO0]\/)\\[oO0]/,
+    title: 'High five ✋',
+    marking: 'o/\\o',
+  },
+  'low-five': {
+    left: /[oO0]{\\}(?!\/[oO0])/,
+    right: /(?<![oO0]\\)\/[oO0]/,
+    title: 'Low five ✋',
+    marking: 'o\\\\/o',
+  },
+  hug: {
+    left: /[oO0][{[(](?![)\]}][oO0])/,
+    right: /(?<![oO0][{[(])[)\]}][oO0]/,
+    title: 'Hug 🤗',
+    marking: 'o{}o',
+  },
+};
+
+const isFive = (message) => {
+  const txt = message.content;
+  const channel = message.channel.id;
+  const author = message.author.id;
+  const { createdAt } = message;
+  for (let i = 0; i < (Object.keys(fives).length); i += 1) {
+    const o = Object.keys(fives)[i];
+    if (fives[o].right.exec(txt)) {
+      return {
+        name: o,
+        side: 'right',
+        channel,
+        author,
+        createdAt,
+      };
+    } if (fives[o].left.exec(txt)) {
+      return {
+        name: o,
+        side: 'left',
+        channel,
+        author,
+        createdAt,
+      };
+    }
+  }
+  return false;
+};
+
+
+module.exports = {
+  messageCondition(message) {
+    return isFive(message);
+  },
+  callback: async (message, client) => {
+    const whichFive = isFive(message);
+    const matchingFives = pendingFives.filter(el => el.channel === whichFive.channel
+         && el.name === whichFive.name
+         && el.side !== whichFive.side
+         && (isDev || (el.author !== whichFive.author)));
+    if (Array.isArray(matchingFives) && matchingFives.length) {
+      // If matching pending five(s) were found
+      for (let i = 0; i < matchingFives.length; i += 1) {
+        /* eslint-disable no-await-in-loop */
+        const pingMsg = {
+          title: fives[whichFive.name].title,
+          description: `${await client.fetchUser(matchingFives[i].author)} ${fives[whichFive.name].marking} ${await client.fetchUser(whichFive.author)}`,
+          color: config.colors[0],
+          image: {
+            url: await giphyFunctions.searchGiphy(whichFive.name),
+          },
+          footer: {
+            icon_url: 'https://i.imgur.com/rWk9hxP.png',
+            text: 'Powered by GIPHY',
+          },
+        };
+        /* eslint-disable no-await-in-loop */
+        await message.channel.send({ embed: pingMsg });
+        pendingFives = pendingFives.filter(el => !(el.channel === matchingFives[i].channel
+            && el.name === matchingFives[i].name
+            && el.side === matchingFives[i].side
+            && el.author === matchingFives[i].author
+            && el.createdAt === matchingFives[i].createdAt));
+      }
+    } else {
+      // If no matching five is pending
+      pendingFives.push(whichFive);
+    }
+    return true;
+  },
+};
--- services/giphy.js
+++ services/giphy.js
@@ -5,11 +5,23 @@ const logger = require('./logger.js');
 // Connecting to GIPHY
 const giphy = giphyRequire(config.giphy.key);
 
+const giphyCache = [];
+
+const findAllGiphUrls = async (keyword) => {
+  if (giphyCache[keyword]) {
+    return giphyCache[keyword];
+  }
+  const allGiphs = await giphy.search(keyword);
+  const allUrls = allGiphs.data.map(a => a.images.original.url);
+  giphyCache[keyword] = allUrls;
+  return allUrls;
+};
+
 module.exports = {
   async searchGiphy(keyword) {
     try {
-      const foundGiphy = await giphy.search(keyword);
-      return foundGiphy.data[Math.floor(Math.random() * 25)].images.original.url;
+      const allUrls = await findAllGiphUrls(keyword);
+      return allUrls[Math.floor(Math.random() * 25)];
     } catch (e) {
       logger.error(`Error while searching for GIF. ${e}`);
     }
